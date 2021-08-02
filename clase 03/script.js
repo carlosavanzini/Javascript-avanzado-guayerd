c16f40b05ab4e033c8b52e57529d8e245c0df918
carlosavanzini <avanzinidesarrollo@gmail.com>
2021-08-02 02:53:02 -0300

    commit

--- clase 03/script.js
+++ clase 03/script.js
@@ -19,53 +19,92 @@
 
 
 //4) En base al siguiente listado de notas, mostrar en verde los mayores a 7, amarillo los que están entre 4 y 6 y en rojo del tres para abajo.
-    notas = [9,2,7,6,6,4,5,10,3,9.5]
+    // notas = [9,2,7,6,6,4,5,10,3,9.5]
 
-    let mayores = notas.filter(a=>a>7);
-    let amarillos = notas.filter(a=>a>=4 && a<=6);
-    let rojos = notas.filter(a=>a<3);
-
-
-    document.write("<h1>Mayores a 7:</h1>");
-    
-    mayores.forEach(a=>document.write("<li class='verdeColor'>" + a + "</li>"));
-    document.querySelectorAll(".verdeColor").forEach(a=>a.classList.add("verdeColor"));
-
-    document.write("<h1>Los que estan entre 4 y 6:</h1>");
-    amarillos.forEach(a=>document.write("<li class='amarillo'>" + a + "</li>"));
-    document.querySelectorAll(".amarillo").forEach(a=>a.classList.add("amarillo"));
-
-    document.write("<h1>Menores de 3:</h1>");
-    rojos.forEach(a=>document.write("<li class='rojoColor'>" + a + "</li>"));
-    document.querySelectorAll(".rojoColor").forEach(a=>a.classList.add("rojoColor"));
-
-   
-
-    
-    
+    // let mayores = notas.filter(a=>a>7);
+    // let amarillos = notas.filter(a=>a>=4 && a<=6);
+    // let rojos = notas.filter(a=>a<3);
 
 
+    // document.write("<h1>Mayores a 7:</h1>");
+    // mayores.forEach(a=>document.write("<li class='verdeColor'>" + a + "</li>"));
+    // document.querySelectorAll(".verdeColor").forEach(a=>a.classList.add("verdeColor"));
 
-//  Tenemos el siguiente HTML. Vamos a preguntarle al usuario qué color quiere cambiar, y luego lo vamos a preguntar por cuál. (Dos input). En base a eso, nuestro javascript modificará el documento. Ejemplo: Si pide cambiar el verde por azul, los textos que antes se veían en verde ahora deberán verse en azul.
+    // document.write("<h1>Los que estan entre 4 y 6:</h1>");
+    // amarillos.forEach(a=>document.write("<li class='amarillo'>" + a + "</li>"));
+    // document.querySelectorAll(".amarillo").forEach(a=>a.classList.add("amarillo"));
+
+    // document.write("<h1>Menores de 3:</h1>");
+    // rojos.forEach(a=>document.write("<li class='rojoColor'>" + a + "</li>"));
+    // document.querySelectorAll(".rojoColor").forEach(a=>a.classList.add("rojoColor"));
+
+
+//5) Tenemos el siguiente HTML. Vamos a preguntarle al usuario qué color quiere cambiar, y luego lo vamos a preguntar por cuál. (Dos input). En base a eso, nuestro javascript modificará el documento. Ejemplo: Si pide cambiar el verde por azul, los textos que antes se veían en verde ahora deberán verse en azul.
 // (Nota: Se debe agregar el css correspondiente).
  
-// <p class="rojo">Lorem ipsum dolor sit amet.</p>
-// <p class="verde"> Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+//  <p class="rojo">Lorem ipsum dolor sit amet.</p>
+//  <p class="verde"> Lorem ipsum dolor sit amet consectetur adipisicing.</p>
 //  <p class="azul"> Lorem ipsum dolor sit amet consecteturadipisicing elit. Suscipit!</p>
 //  <p class="verde"> Lorem, ipsum.</p>
 //  <p class="azul"> Lorem, ipsum dolor.</p>
 //  <p class="verde"> Lorem ipsum dolor sit.</p>
-// Pedir al usuario: Una palabra, un color y una cantidad. Generar la cantidad de párrafos con esa palabra y ese color que se requiera.
 
-// a) Dentro del siguiente HTML, a todos los párrafos que NO tengan la clase “nuevo”, agregarle la clase “antiguo”.
+
+
+
+   
+
+function cambiar(){ 
+    let color = prompt("¿Qué color quieres cambiar?");
+    let colorAcambiar = prompt("¿A qué color quieres cambiar?");
+    
+    if (color == "rojo" && colorAcambiar==="azul") {    
+        document.querySelectorAll("p").forEach(a=>a.classList.replace('rojo','azul'))}
+    if (color == "rojo" && colorAcambiar==="verde") {    
+        document.querySelectorAll("p").forEach(a=>a.classList.replace('rojo','verde'))}
+
+    if (color == "azul" && colorAcambiar==="verde") {
+        document.querySelectorAll("p").forEach(a=>a.classList.replace('azul','verde'))}
+    if (color == "azul" && colorAcambiar==="rojo") {
+        document.querySelectorAll("p").forEach(a=>a.classList.replace('azul','rojo'))}
+
+    if (color == "verde" && colorAcambiar==="rojo") {
+        document.querySelectorAll("p").forEach(a=>a.classList.replace('verde','rojo'))}
+    if (color == "verde" && colorAcambiar==="azul") {
+        document.querySelectorAll("p").forEach(a=>a.classList.replace('verde','azul'))}
+}
+    
+cambiar();
+
+
+
+
+    
+   
+
+  
+
+
+
+
+
+//6) Pedir al usuario: Una palabra, un color y una cantidad. Generar la cantidad de párrafos con esa palabra y ese color que se requiera.
+
+//7) a) Dentro del siguiente HTML, a todos los párrafos que NO tengan la clase “nuevo”, agregarle la clase “antiguo”.
 // b) A todos los que tienen clase “rojo”, agregarles la clase “transparencia” 
 // c) A todos los que tengan clase “saludo” quitarla, y a los que no la tengan, agregarla. 
 // d) Remover de los primeros 4 párrafos la clase “ayuda”
 // e) Crear desde el CSS estilos personalizados para cada clase.
-// Dado el siguiente HTML, poner en color azul los textos que comiencen con la letra “V”.
-// <span>Viento</span>
-// <span>Balanza</span>
-// <span>Zapato</span>
-// <span>Vertical</span>
-// <span>Vuelta</span>
-// <span>Trampa</span>
+/* <p class="nuevo rojo saludo"></p>
+<p class="rojo saludo"></p>
+<p class="campo"></p>
+<p class="nuevo saludo ayuda"></p>
+<p class="nuevo ayuda"></p> */
+
+//8)Dado el siguiente HTML, poner en color azul los textos que comiencen con la letra “V”.
+/* <span>Viento</span>
+<span>Balanza</span>
+<span>Zapato</span>
+<span>Vertical</span>
+<span>Vuelta</span>
+<span>Trampa</span> */
