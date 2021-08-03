974c9b70bfc62520384a1ccd9212ece347daca54
carlosavanzini <avanzinidesarrollo@gmail.com>
2021-08-02 21:05:14 -0300

    commit

---
+++ clase 04/script.js
@@ -0,0 +1,43 @@
+// b.addEventListener("click",adios)
+// click, blur, mouseover, click, focus, change,
+
+
+// Ejercicios de eventos
+
+// Crear un botón que al hacerle click, muestre un saludo por alert.
+
+    // document.write("<button id='saludar'>click para que te salude</button>")
+    // let e= document.querySelector("#saludar")
+    // let saludo=()=>alert("Hola a todos")
+
+    // e.addEventListener("click",saludo)
+
+// Crear un botón que al hacerle click, muestre un mensaje en el HTML.
+
+    // document.write("<button id='saludar'>click para para que te muestre un mensaje en el html</button>")
+    // document.write("<p id='forma'></p>")
+    //     let e= document.querySelector("#saludar")
+    //     let saludo=()=>document.querySelector("#forma").innerText="hola a todos"
+
+    //     e.addEventListener("click",saludo)
+
+// Crear un botón que al hacerle click cambie de color.
+
+    // document.write("<button class='verde'>click para que este boton cambie de color</button>")
+
+    // let e= document.querySelector(".verde")
+    // let saludo=()=>document.querySelector(".verde").classList.replace("verde","rojo")
+
+    // e.addEventListener("click",saludo)
+
+    
+
+// Crear 5 botones numerados del 1 al 5. Al hacer click en uno y mostrar un mensaje que diga “Haz clickeado el botón $número”, indicando al usuario cuál botón apretó.
+// Crear un input y un botón. Dentro del input el usuario debe ingresar 
+// Crear dos input tipo número y un botón. 
+// a) Al hacer foco en un input se debe poner azul.
+// b) Al escribir, se debe poner violeta.
+// c) Cuando se le saca el foco, se pone verde.
+// d) Al hacer click en el boton genera un nuevo párrafo en el html que muestra la suma de ambos
+// Poner en un select tres (o más) opciones de superheroes. Dependiendo el que el usuario elija, se mostrará debajo una imagen que haga referencia al personaje. Cuando se le pase el mouse por encima, la imagen debe mostrar una foto de su identidad secreta en su lugar. (Por ejemplo en lugar de verse superman, se vera a Clark Kent).
+// Investigar y elegir un evento que no hayamos visto en clase. Hacer una demo de cómo funciona. 
